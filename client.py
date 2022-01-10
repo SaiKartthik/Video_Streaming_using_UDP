c2e34e1268c828b4f464d6ce622d3630dc992a3d
Sai Karthik <84378022+SaiKartthik@users.noreply.github.com>
2022-01-10 22:47:46 +0530

    Updated Client.py

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+#Importing required packages
 from socket import socket
 import cv2
 import socket
@@ -7,9 +8,59 @@ from PIL import Image, ImageTk
 import base64
 import numpy as np
 import time
+import wave,pyaudio
+import threading,queue
+
+#Method to decode information received from Server
+def decode1(bb):
+    bb=bb.decode('ASCII')
+    return int(bb)
 thumbnaillist=[]
-def video_stream(b):
-    print(b)
+q = queue.Queue(maxsize=2000)
+
+#Joining Audio and Video
+def parallel(i):
+    from concurrent.futures import ThreadPoolExecutor
+    with ThreadPoolExecutor(max_workers=2) as executor1:
+        executor1.submit(video,(i))
+        executor1.submit(audio,(i))
+
+#Get Audio and Related data
+def audio(b1):
+    print(b1)
+    audioclientsocket=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    audioclientsocket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
+    message12=b1[0:len(b1)-4]+".wav"
+    print(message12)
+    message12=message12.encode('ASCII')
+    audioclientsocket.sendto(message12,(host_ip,port-1))
+    byte_len,_=audioclientsocket.recvfrom(BUFF_SIZE)
+    n=byte_len.decode('ASCII')
+    n1=int(n)
+    p = pyaudio.PyAudio()
+    CHUNK = 10*1024
+    stream = p.open(format=p.get_format_from_width(2),channels=2,rate=44100,output=True,frames_per_buffer=CHUNK)
+    cnt1=0
+
+    def getAudioData():
+        while cnt1<(n1/CHUNK):
+            frame,_= audioclientsocket.recvfrom(BUFF_SIZE)
+            q.put(frame)
+            print('Queue size...',q.qsize())
+            cnt1+1
+    t1 = threading.Thread(target=getAudioData, args=())
+    t1.start()
+    time.sleep(1)
+    print('Now Playing...')
+    cnt2=0
+    while cnt2<(n1/CHUNK):
+        frame = q.get()
+        stream.write(frame)
+        cnt2+=1
+    print('Audio closed')
+
+#Get video and related data
+def video(b):
     b=b.encode('ASCII')
     client_socket.sendto(b,(host_ip,port))
     lenth,_=client_socket.recvfrom(BUFF_SIZE)
@@ -17,6 +68,7 @@ def video_stream(b):
     print(lenth)
     fps,st,frames_to_count,cnt = (10,0,20,0)
     cnt1=0
+
     while cnt1<lenth:
         packet,_ = client_socket.recvfrom(BUFF_SIZE)
         data = base64.b64decode(packet,' /')
@@ -40,48 +92,51 @@ def video_stream(b):
         if cnt1==lenth:
             cv2.destroyAllWindows()
             break
-    print("working")
 
-
-
+#To destroy sockets
 def on_closing(tp):
     print("closed")
     tp.destroy()
     client_socket.close()
-def addthumbnail(tp,b,i):
-    label12=Button(tp,text=i,command=lambda:video_stream(i)).place(x=0,y=b)
+
+#Adding images to thumbnails
+def addthumbnail(tp,b,i,img):
+    label12=Button(tp,image=img,command=lambda:parallel(i)).place(x=0,y=b)
+
+#Method to display files in a GUI
 def gui():
     tp=Tk()
     tp.geometry("500x500")
     tp.protocol("WM_DELETE_WINDOW",lambda:on_closing(tp))
     a=0
     b=0
+
     for i in thumbnaillist:
-        addthumbnail(tp,b,i)
         image = Image.open(i)
-        resize_image = image.resize((200,200))
+        resize_image = image.resize((100,100))
         img = ImageTk.PhotoImage(resize_image)
-        b=b+20
+        addthumbnail(tp,b,i,img)
+        b=b+110
     sb = Scrollbar(tp)
     sb.pack( side = RIGHT,fill = Y )
     tp.mainloop()
+
 BUFF_SIZE=65536
 client_socket=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 client_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
 host_ip=socket.gethostname()
-port=9688
+port=9999
 message="hello"
 message=message.encode('ASCII')
 client_socket.sendto(message,(host_ip,port))
 x,_=client_socket.recvfrom(BUFF_SIZE)
 x=int.from_bytes(x,'big')
-print(x)
+print("No of files found : ",x)
 cnt=0
+
 while cnt<x:
     y,_=client_socket.recvfrom(BUFF_SIZE)
     y=y.decode('ASCII')
     thumbnaillist.append(y)
     cnt+=1
 gui()
-
-#
